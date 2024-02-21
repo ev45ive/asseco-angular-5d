e423bea3aba2ade6d4c1f48e88246819045750fb
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-21 14:44:24 +0100

    NgZone, ApplicationRef.tick(), runOutsideAngular()

--- src/app/shared/components/clock/clock.component.ts
+++ src/app/shared/components/clock/clock.component.ts
@@ -1,8 +1,10 @@
 import { isPlatformServer } from '@angular/common';
 import {
+  ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   Inject,
+  NgZone,
   PLATFORM_ID,
 } from '@angular/core';
 
@@ -11,27 +13,31 @@ import {
   styleUrl: './clock.component.scss',
   // templateUrl: './clock.component.html',
   template: `<span>Time: {{ time }}</span>`,
-  // changeDetection: ChangeDetectionStrategy.OnPush // ☊ d[-_-]b
+  changeDetection: ChangeDetectionStrategy.OnPush, // ☊ d[-_-]b
 })
 export class ClockComponent {
   time = new Date().toLocaleTimeString();
 
   constructor(
     @Inject(PLATFORM_ID) private pid: object,
-    readonly cdr: ChangeDetectorRef
+    private zone: NgZone,
+    private cdr: ChangeDetectorRef
   ) {
-    cdr.detach();
+    // cdr.detach();
   }
 
   ngOnInit(): void {
-    this.cdr.detectChanges();
+    // this.cdr.detectChanges();
 
     if (isPlatformServer(this.pid)) return;
 
+    // don't do ApplicationRef.tick()
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.time = new Date().toLocaleTimeString();
+        this.cdr.detectChanges();
+      }, 1_000);
+    });
     /* NG0506: Angular hydration expected the ApplicationRef.isStable() to emit `true`, but it didn't happen within 10000ms. Angular hydration logic depends on the application becoming stable as a signal to complete hydration process. Find more at https://angular.io/errors/NG0506 */
-    setInterval(() => {
-      this.time = new Date().toLocaleTimeString();
-      // this.cdr.detectChanges();
-    }, 1_000);
   }
 }
