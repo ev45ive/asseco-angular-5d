a335b4700c8a68fe4a6782d916c0c414b3debff7
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-21 14:41:35 +0100

    setInterval + Zone.JS - ZOne - transaction

--- src/app/shared/components/clock/clock.component.ts
+++ src/app/shared/components/clock/clock.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
 
 @Component({
   selector: 'app-clock',
@@ -10,16 +16,22 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 export class ClockComponent {
   time = new Date().toLocaleTimeString();
 
-  constructor(readonly cdr: ChangeDetectorRef) {
+  constructor(
+    @Inject(PLATFORM_ID) private pid: object,
+    readonly cdr: ChangeDetectorRef
+  ) {
     cdr.detach();
   }
 
   ngOnInit(): void {
     this.cdr.detectChanges();
-    
+
+    if (isPlatformServer(this.pid)) return;
+
+    /* NG0506: Angular hydration expected the ApplicationRef.isStable() to emit `true`, but it didn't happen within 10000ms. Angular hydration logic depends on the application becoming stable as a signal to complete hydration process. Find more at https://angular.io/errors/NG0506 */
     setInterval(() => {
       this.time = new Date().toLocaleTimeString();
-      this.cdr.detectChanges();
+      // this.cdr.detectChanges();
     }, 1_000);
   }
 }
