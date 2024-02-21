49632b518437e8f2f597de40c8bd7970dbc61bdb
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-21 11:58:55 +0100

    Routes, path, redirectTo , pathMatch, '**' - catchAll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'playlists',
+    pathMatch: 'full', // match full length
+  },
+  {
     path: 'playlists',
     loadChildren: () =>
       import('./playlists/playlists.module').then((m) => m.PlaylistsModule),
+  },
+  {
+    path: '**',
+    // component: PageNotFound // TODO
+    redirectTo: 'playlists',
+    pathMatch: 'full', // match full length
   },
 ];
 
