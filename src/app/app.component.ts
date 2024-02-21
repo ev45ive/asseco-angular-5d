41e4d14d6d73739949544532d458386c6689439f
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-21 10:47:23 +0100

    Component Decorator, Selector, Template, Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root, .placki[sos=malinowy]',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'asseco';
+
+  login(){
+    this.title = 'Admin'
+  }
 }
