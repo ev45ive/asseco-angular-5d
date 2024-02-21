a2647d541b342b0c25262213e9f2fccef2d2004d
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-21 15:06:10 +0100

    Exercise: Switching modes - toogle detail/editor with buttons

--- src/app/playlists/containers/playlists-view/playlists-view.component.ts
+++ src/app/playlists/containers/playlists-view/playlists-view.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlists-view',
   templateUrl: './playlists-view.component.html',
-  styleUrl: './playlists-view.component.scss'
+  styleUrl: './playlists-view.component.scss',
 })
 export class PlaylistsViewComponent {
+  mode: 'details' | 'editor' = 'details';
 
+  showDetails() {
+    this.mode = 'details';
+  }
+  
+  showEditor() {
+    this.mode = 'editor';
+  }
 }
