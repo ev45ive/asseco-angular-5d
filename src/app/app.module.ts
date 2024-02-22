3077292459ed6d54ddfee6ad797fe55b431ee330
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-22 09:24:23 +0100

    ngDoBootstrap(appRef: ApplicationRef)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +10,18 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule{}
+
+// export class AppModule implements DoBootstrap {
+//   ngDoBootstrap(appRef: ApplicationRef): void {
+//     // appRef.tick()
+//     const comp = appRef.bootstrap(AppComponent, 'app-root');
+//     // comp.destory()
+//   }
+// }
