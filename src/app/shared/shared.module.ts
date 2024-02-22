1110b5cfe1f8c02eeabeefac32889676d00ef050
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-22 12:32:40 +0100

    [ngModel]="playlist.name" (ngModelChange)="playlist.name = $event"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { YesnoPipe } from './pipes/yesno.pipe';
 import { ClockComponent } from './components/clock/clock.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,9 +14,11 @@ import { ClockComponent } from './components/clock/clock.component';
   imports: [
     CommonModule,
     NgOptimizedImage,
+    FormsModule,
   ],
   exports: [
     NgOptimizedImage,
+    FormsModule,
     CommonModule,
     YesnoPipe,
     ClockComponent
