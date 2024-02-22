e130b20ebf50509a7439cef188341af8be1405ae
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-22 11:38:35 +0100

    <ng-template ngFor [ngForOf]="playlists" let-playlist="$implicit" let-i="index">

--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -1,11 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { PlaylistsViewComponent } from '../../containers/playlists-view/playlists-view.component';
-import { AppComponent } from '../../../app.component';
-import { Router } from '@angular/router';
+
+NgFor
 
 @Component({
   selector: 'app-playlist-list',
   templateUrl: './playlist-list.component.html',
   styleUrl: './playlist-list.component.scss',
 })
-export class PlaylistListComponent {}
+export class PlaylistListComponent {
+
+  playlists = [
+    {
+      id: '123',
+      name: 'Playlist 123',
+      public: false,
+      description: 'Best playlist',
+    },
+    {
+      id: '234',
+      name: 'Playlist 234',
+      public: true,
+      description: 'Cool playlist',
+    },
+    {
+      id: '345',
+      name: 'Playlist 345',
+      public: false,
+      description: 'Awesome playlist',
+    },
+  ];
+}
