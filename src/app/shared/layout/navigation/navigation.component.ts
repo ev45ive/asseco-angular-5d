0028edfee60112e3707e178de392f033e1cae254
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-23 10:25:11 +0100

    @ContentChild, @ContentChildren, QueryList<ElementRef<HTMLElemen>>>

--- src/app/shared/layout/navigation/navigation.component.ts
+++ src/app/shared/layout/navigation/navigation.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ContentChildren,
+  ElementRef,
+  QueryList,
+} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
-  open = false
+  open = false;
+
+  // @ContentChild
+  @ContentChildren('appLink')
+  appLinks?: QueryList<ElementRef<HTMLElement>>
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    const links = this.appLinks?.toArray()
+  }
 }
