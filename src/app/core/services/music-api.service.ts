5c52bb35f539bbbff69e305245cdfb0c44bd898f
Mateusz Kulesza <ev45ive@gmail.com>
2024-02-29 12:26:12 +0100

    Exponential backoff retry error handling

--- src/app/core/services/music-api.service.ts
+++ src/app/core/services/music-api.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, inject } from '@angular/core';
+import { ErrorHandler, Injectable, inject } from '@angular/core';
 import mockAlbums from '../mocks/mockAlbums';
 import { API_URL } from '../tokens';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { AlbumResponse, AlbumSearchResponse } from '../model/Album';
 import {
   EMPTY,
@@ -11,9 +11,13 @@ import {
   from,
   map,
   of,
+  retry,
+  switchMap,
   throwError,
+  timer,
 } from 'rxjs';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { time } from 'console';
 
 @Injectable({
   providedIn: 'root',
@@ -22,6 +26,8 @@ export class MusicAPIService {
   api_url = inject(API_URL);
   http = inject(HttpClient);
   oauth = inject(OAuthService);
+  // TODO: OpenTelemetry - Sentry, Grafana, Splunk, DataDog?
+  errorHandler = inject(ErrorHandler);
 
   search(query = '') {
     console.log('Searching.... ', this.api_url, query);
@@ -38,15 +44,28 @@ export class MusicAPIService {
       })
       .pipe(
         map((res) => res.albums.items),
-        catchError((error, catchedObservable) => {
-          // return from([]); // --|>
-          // return []; // --|>
-          // return [mockAlbums, mockAlbums]; // --OO|>
-          // return EMPTY // --|>
-          // return this.http.get('inny server')
+        retry({
+          delay(error: unknown, retryCount) {
+            const RETRY_STATUS_CODES = [408, 413, 429, 500, 502, 503, 504, 0];
 
-          // Retry:
-          // return catchedObservable
+            if (
+              !(error instanceof HttpErrorResponse) ||
+              !RETRY_STATUS_CODES.includes(error.status) ||
+              retryCount <= 3
+            )
+              return timer(500 * retryCount ** 2);
+
+            return throwError(() => error);
+          },
+        }),
+        catchError((error, catchedObservable) => {
+          this.errorHandler.handleError(error);
+
+          if (!(error instanceof HttpErrorResponse))
+            return throwError(() => new Error('Unexpected error'));
+
+          if (!error.status) // TODO: Retry when connection returns
+            return throwError(() => new Error('No internet connection'));
 
           return throwError(() => new Error(error.error.error.message));
         }),
