947dcf94a16ced070d7238ed8c0c9e63f435df65
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-01 09:38:00 +0100

    routerLink, routerLinkActive, isActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      // useHash: true, // useHistory pushState or Hash
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
