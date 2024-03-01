0449f3002ee9aeaa7bdb57c0663310770c89a305
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-01 12:18:35 +0100

    <ng-container *ngIf="searchChanges | async as results">

--- src/app/music/containers/album-search-view/album-search-view.component.ts
+++ src/app/music/containers/album-search-view/album-search-view.component.ts
@@ -11,7 +11,7 @@ import { ResultsGridComponent } from '../../components/results-grid/results-grid
 import { MusicAPIService } from '../../../core/services/music-api.service';
 import { Album, AlbumResponse } from '../../../core/model/Album';
 import { ActivatedRoute, Router } from '@angular/router';
-import { isPlatformServer } from '@angular/common';
+import { AsyncPipe, NgIf, isPlatformServer } from '@angular/common';
 import {
   EMPTY,
   Observable,
@@ -34,35 +34,21 @@ import { NotificationsService } from '../../../core/services/notifications.servi
   templateUrl: './album-search-view.component.html',
   styleUrl: './album-search-view.component.scss',
   standalone: true,
-  imports: [SearchFormComponent, ResultsGridComponent],
+  imports: [SearchFormComponent, NgIf, AsyncPipe, ResultsGridComponent],
 })
 export class AlbumSearchViewComponent {
   api = inject(MusicAPIService);
   router = inject(Router);
   route = inject(ActivatedRoute);
-  pid = inject(PLATFORM_ID);
-
-  query: string | null = '';
-  message = '';
-  results: Album[] = [];
 
   queryChanges = this.route.queryParamMap.pipe(
     map((pm) => pm.get('q')),
     filter(Boolean),
-    takeUntilDestroyed(),
   );
 
   searchChanges = this.queryChanges.pipe(
     switchMap((query) => this.api.search(query)),
-    takeUntilDestroyed(), // After requests!
   );
-
-  ngOnInit(): void {
-    if (isPlatformServer(this.pid)) return;
-
-    this.queryChanges.subscribe((q) => (this.query = q));
-    this.searchChanges.subscribe((albums) => (this.results = albums));
-  }
 
   searchAlbums(query = '') {
     this.router.navigate([], {
