97b0cb356d64f3905aa352b4cc44dead1d55c5fd
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-01 15:08:37 +0100

    setTimeout + afterRender

--- src/app/music/containers/album-detail-view/album-detail-view.component.ts
+++ src/app/music/containers/album-detail-view/album-detail-view.component.ts
@@ -1,9 +1,17 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  afterRender,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { AlbumCardComponent } from '../../components/album-card/album-card.component';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs';
 import { MusicAPIService } from '../../../core/services/music-api.service';
 import { AsyncPipe, DatePipe } from '@angular/common';
+import { Track } from '../../../core/model/Album';
 
 @Component({
   selector: 'app-album-detail-view',
@@ -13,6 +21,21 @@ import { AsyncPipe, DatePipe } from '@angular/common';
   styleUrl: './album-detail-view.component.scss',
 })
 export class AlbumDetailViewComponent {
+  selectedTrack?: Track;
+
+  // audioRef = viewChild<ElementRef<HTMLAudioElement>>('audioRef');
+
+  @ViewChild('audioRef')
+  audioRef?: ElementRef<HTMLAudioElement>;
+
+  play(track: Track) {
+    this.selectedTrack = track;
+    setTimeout(() => this.audioRef?.nativeElement.play());
+
+    // Only in injection context
+    // afterRender(() => this.audioRef?.nativeElement.play());
+  }
+
   route = inject(ActivatedRoute);
   albumId = this.route.paramMap.pipe(
     map((pm) => pm.get('albumId')),
